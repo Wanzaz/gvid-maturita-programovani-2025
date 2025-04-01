32a2ff156f5313e30eb4ab70620fc466bb8795a3
Wanzaz <ondra.pazourek@gmail.com>
2025-04-01 19:55:19 +0200

    Predelava do struktury - hornerovo schema

--- 00-Banka-uloh/polynom.c
+++ 00-Banka-uloh/polynom.c
@@ -1,26 +1,32 @@
 #include <stdio.h>
 
+typedef struct _polynom {
+    int stupen;
+    int koef[50];
+} TPolynom;
+
 // načti ze vstupu stupeň a koeficienty polynomu, dále načti hodnotu x a vypočítej Hornerovým schématem hodnotu polynomu v daném bodě
 
-void nactiPolynom(FILE *f, int koef[], int stupen)
+void nactiPolynom(FILE *f, TPolynom* p)
 {
-    for (int i = 0; i <= stupen; i++) {
-        fscanf(f, "%d", &koef[i]); // POZOR ne naivne "%d " - BEZ MEZERY
+    fscanf(f, "%d", &p->stupen); // Nejprve načteme stupeň!
+    for (int i = 0; i <= p->stupen; i++) {
+        fscanf(f, "%d", &p->koef[i]); // POZOR ne naivne "%d " - BEZ MEZERY
     }
 }
 
-void vytiskniPolynom(FILE *f, int koef[], int stupen)
+void vytiskniPolynom(FILE *f, TPolynom p)
 {
-    for (int i = 0; i <= stupen; i++) {
-        fprintf(f, "%d ", koef[i]);
+    for (int i = 0; i <= p.stupen; i++) {
+        fprintf(f, "%d ", p.koef[i]);
     }
 }
 
-int horner(int koef[], int x, int stupen)
+int horner(TPolynom p, int x)
 {
     int sum = 0;
-    for (int i = 0; i <= stupen; i++) {
-        sum = sum*x + koef[i];
+    for (int i = 0; i <= p.stupen; i++) {
+        sum = sum*x + p.koef[i];
     }
 
     return sum;
@@ -29,20 +35,16 @@ int horner(int koef[], int x, int stupen)
 
 int main(int argc, char *argv[])
 {
-    int koef[100];
-    int stupen;
+    TPolynom p;
     int x;
 
 
-    printf("Zadej stupen polynomu: ");
-    scanf("%d", &stupen);
     printf("Zadej bod pro vycisleni: ");
     scanf("%d", &x);
-    printf("\nZadej koeficienty polynomu: \n");
-    nactiPolynom(stdin, koef, stupen);
+    printf("\nZadej stupen polynomu a koeficienty polynomu: \n");
+    nactiPolynom(stdin, &p);
 
-    printf("Vycisleni polynomu v bode %d: %d\n", x, horner(koef, x, stupen));
-    /* vytiskniPolynom(stdout, koef, stupen); */
+    printf("Vycisleni polynomu v bode %d: %d\n", x, horner(p, x));
 
     return 0;
 }
