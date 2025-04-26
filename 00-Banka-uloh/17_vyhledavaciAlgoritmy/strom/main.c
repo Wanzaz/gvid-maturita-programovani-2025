42ddf6b59768c8cabf334b8f7c57fb2282a96fd6
Wanzaz <ondra.pazourek@gmail.com>
2025-04-26 11:27:39 +0200

    Add tree ADT to 09 and 17

---
+++ 00-Banka-uloh/17_vyhledavaciAlgoritmy/strom/main.c
@@ -0,0 +1,507 @@
+#include <stdio.h>
+#include <stdlib.h>
+// #include <string.h>  // pro praci s textovymi retezci
+#include <stdbool.h> // pro praci s typem bool a konstantami true a false
+#include <ctype.h>   // isalpha, isspace, islower, isupper, ...
+ #include <time.h>    // funkce time a dalsi pro praci s casem
+#include "SuperTisk.h"
+#include "strom.h"
+
+/**
+Doplň program (bude k dispozici kostra programu) pro práci s binárním vyhledávacím
+stromem obsahujícím celočíselné klíče. Napiš podprogram pro
+• nalezení minimálního / maximálního prvku ve stromu
+• nalezení zadaného klíče
+• výpis klíčů seřazených vzestupně / sestupně
+• přidání nového klíče do stromu
+• výpis všech klíčů menších / větších než zadaný klíč
+• zrušení celého stromu
+• zrušení listu
+• výpočet váhy stromu (počtu prvků ve stromu)
+• výpočet výšky stromu (největší vzdálenost kořen – list) **/
+
+/** Uzel binárního stromu. Dvojice klic-data je zde int-float. 
+typedef struct _tuzel Tuzel;
+// Vnitřek typu Tuzel je ukryt v souboru strom.c.
+
+
+// Struktura reprezentující celý strom. 
+typedef struct _strom Tstrom;
+
+// Tohle uživatel vidět nemusí. Ještě by mi do toho vlezl a něco mi tu poničil.
+struct _tuzel
+{
+    int klic;           //< Klíč pro vyhledávání -- zde celé číslo
+    float data;         //< Data vázaná na klíč -- zde desetinné číslo
+    Tuzel* levy;   //< Ukazatel na levý podstrom
+    Tuzel* pravy;  //< Ukazatel na pravý podstrom
+};
+
+struct _strom
+{
+    Tuzel* koren;
+    int vaha;
+};
+**/
+
+/* nalezení minimálního ve stromu*/
+Tuzel *_najdiMinimum(Tuzel *u)
+{
+    if (u == NULL) return NULL;
+    if (u->levy == NULL) return u;
+    return _najdiMinimum(u->levy);
+}
+
+Tuzel *najdiMinimum(Tstrom *strom)
+{
+    if (strom == NULL || strom->koren == NULL) return NULL;
+    return _najdiMinimum(strom->koren);
+}
+
+
+/* maximálního prvku ve stromu */
+Tuzel *_najdiMaximum(Tuzel *u)
+{
+    if (u == NULL) return NULL;
+    if (u->pravy == NULL) return u;
+    return _najdiMaximum(u->pravy);
+}
+
+Tuzel *najdiMaximum(Tstrom *strom)
+{
+    if (strom == NULL || strom->koren == NULL) return NULL;
+    return _najdiMaximum(strom->koren);
+}
+
+// výpis klíčů seřazených vzestupně
+void _inOrder(Tuzel * u)
+{
+    if (u == NULL) { return; }
+
+    _inOrder(u->levy);
+    printf("%d - %f\n", u->klic, u->data);
+    _inOrder(u->pravy);
+}
+
+void vypisInOrder(Tstrom * strom)
+{
+    _inOrder(strom->koren);
+}
+
+// výpis klíčů seřazených vzestupně / sestupně
+void _inOrderReverse(Tuzel * u)
+{
+    if (u == NULL) { return; }
+
+    _inOrderReverse(u->pravy);
+    printf("%d - %f\n", u->klic, u->data);
+    _inOrderReverse(u->levy);
+}
+
+void vypisInOrderReverse(Tstrom * strom)
+{
+    _inOrderReverse(strom->koren);
+}
+
+// výpočet váhy stromu (počtu prvků ve stromu)
+int _vaha(Tuzel * u)
+{
+    if (u == NULL) { return 0; }
+
+    return 1 + _vaha(u->levy) + _vaha(u->pravy);
+
+}
+
+int bvsPruchodVaha(Tstrom * strom)
+{
+    return _vaha(strom->koren);
+}
+
+// nebo chytre
+int bvsVaha(Tstrom* strom)
+{
+    return strom->vaha;
+}
+
+
+// výpočet výšky stromu (největší vzdálenost kořen – list) **/
+int _vyska(Tuzel *u)
+{
+    if (u == NULL) return -1;
+
+    int leva = _vyska(u->levy);
+    int prava = _vyska(u->pravy);
+
+    return 1 + (leva > prava ? leva : prava);
+}
+
+int vyskaStromu(Tstrom *strom)
+{
+    return _vyska(strom->koren);
+}
+
+void operaceH_vahaVyska(Tstrom *strom)
+{
+    if (bvsVaha(strom) == bvsPruchodVaha(strom)) {
+        printf("\nVyska stromu: %d\n", bvsVaha(strom));
+    } else {
+        printf("Neco se pokazilo. Utikej!!!\n");
+    }
+}
+
+// zrušení celého stromu
+// klasicky POSTORDER
+void _bvsZrus(Tuzel *u)
+{
+    if (u == NULL) return;
+    _bvsZrus(u->levy);
+    _bvsZrus(u->pravy);
+    free(u);
+}
+
+void bvsZrus(Tstrom *strom)
+{
+    _bvsZrus(strom->koren);
+    free(strom);
+}
+
+Tuzel *_novyUzel(int klic, float data)
+{
+    Tuzel *u = malloc(sizeof(Tuzel));
+    if (u == NULL) return NULL;
+    u->klic = klic;
+    u->data = data;
+    u->levy = u->pravy = NULL;
+    return u;
+}
+
+bool _bvsVloz(Tuzel **u, int klic, float data)
+{
+    Tuzel* pom = *u; // pomucka pro eliminaci *, POZOR!!!
+    
+    if (pom == NULL) {
+        if ((pom = _novyUzel(klic, data)) == NULL) { return false; }
+
+        *u = pom; // NUTNE!!! pom je lokalni
+        return true;
+    }
+    // nevkladame nahodou duplicitni klic? ne nevkladamae protoze tady si
+    // na duplicitni klice nehrajeme
+    //if (klic == pom->klic) return false
+    
+    // patri v levo nebo pravo
+    /* if (klic < pom->klic) { */
+    /*     return _bvsVloz(&pom->levy, klic, data); */
+    /* } else { */
+    /*     return _bvsVloz(&pom->pravy, klic, data); */
+    /* } */
+    
+    // lepsi zapsani swag
+    return _bvsVloz(klic < pom->klic ? &pom->levy : &pom->pravy, klic, data);
+}
+
+bool bvsVloz(Tstrom *strom, int klic, float data)
+{
+    if (_bvsVloz(&strom->koren, klic, data))
+    {
+        strom->vaha += 1;
+        return true;
+    }
+    return false;
+}
+
+/* zrušení (vybraného) listu */
+bool _zrusVybranyList(Tuzel **u, int klic)
+{
+    if (*u == NULL) return false;
+
+    Tuzel *pom = *u;
+
+    if (pom->klic == klic)
+    {
+        // Ověření, že to je list
+        if (pom->levy == NULL && pom->pravy == NULL)
+        {
+            free(pom);
+            *u = NULL;
+            return true;
+        }
+        else
+        {
+            // Pokud to není list, nemůžeme mazat
+            return false;
+        }
+    }
+
+    return _zrusVybranyList(klic < pom->klic ? &pom->levy : &pom->pravy, klic);
+}
+
+
+bool zrusVybranyList(Tstrom *strom, int klic)
+{
+    if (strom == NULL || strom->koren == NULL) return false;
+
+    if (_zrusVybranyList(&strom->koren, klic))
+    {
+        strom->vaha -= 1;
+        return true;
+    }
+    return false;
+}
+
+/* zrušení prvního listu */
+bool _zrusPrvniList(Tuzel **u)
+{
+    if (*u == NULL) return false;
+
+    Tuzel *pom = *u;
+
+    if (pom->levy == NULL && pom->pravy == NULL)
+    {
+        free(pom);
+        *u = NULL; // Nastavíme ukazatel na NULL
+        return true;
+    }
+
+    if (pom->levy != NULL) {
+        if (_zrusPrvniList(&pom->levy)) {
+            return true;
+        }
+    }
+
+    if (pom->pravy != NULL) {
+        if (_zrusPrvniList(&pom->pravy)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
+// nalezení zadaného klíče
+Tuzel* _bvsNajdi(Tuzel *u, int klic)
+{
+    if (u == NULL) return NULL;
+
+    if (u->klic == klic) {
+        return u; // Nalezeno!
+    }
+
+    return _bvsNajdi(klic < u->klic ? u->levy : u->pravy, klic);
+}
+
+Tuzel* bvsNajdi(Tstrom *strom, int klic)
+{
+    if (strom == NULL) return NULL;
+    return _bvsNajdi(strom->koren, klic);
+}
+
+
+// výpis všech klíčů menších než zadaný klíč
+void _mensiNez(Tuzel *u, int klic)
+{
+    if (u == NULL) { return; }
+
+    _mensiNez(u->levy, klic);
+    if (klic > u->klic) { printf("%d - %f\n", u->klic, u->data); }
+    _mensiNez(u->pravy, klic);
+}
+
+void vypisMensiNez(Tstrom *strom, int klic)
+{
+    _mensiNez(strom->koren, klic);
+}
+
+// výpis všech klíčů větších než zadaný klíč
+void _vetsiNez(Tuzel *u, int klic)
+{
+    if (u == NULL) { return; }
+
+    _vetsiNez(u->levy, klic);
+    if (klic < u->klic) { printf("%d - %f\n", u->klic, u->data); }
+    _vetsiNez(u->pravy, klic);
+}
+
+void vypisVetsiNez(Tstrom *strom, int klic)
+{
+    _vetsiNez(strom->koren, klic);
+}
+
+void vlozeni10Uzlu(Tstrom *strom)
+{
+    // automaticky vlozit 10 uzlu zdrzuje - FINTAAAAA
+    srand(time(NULL));
+    for (int i = 10; i > 0; i--) {
+        bvsVloz(strom, rand() % 100, i + 0.5); // rand() % 100
+    }
+}
+
+
+
+
+void menu(void)
+{
+    printf(
+        "\n"
+        "============== Hlavní menu ==============\n"
+        "I - Inicializace / zruseni stromu\n"
+        "T - Tisk stromu\n"
+        "V - Vlozeni noveho prvku\n"
+        "O - Odebrani prvku podle klice\n"
+        "K - Konec programu\n"
+        "-----------------------------------------\n"
+        "Rozsirene operace:\n"
+        "8 - Najdi minimum\n"
+        "9 - Najdi maximum\n"
+        "A - Najdi konkretni klic\n"
+        "B - Vypis vsechny klíče < nez zadany\n"
+        "C - Vypis vsechny klíče > nez zadany\n"
+        "D - Smaz list podle klice\n"
+        "E - Vypis klíčů vzestupne (in-order)\n"
+        "F - Vypis klíčů sestupne (reverse in-order)\n"
+        "H - Vypocet vahy a vysky stromu\n"
+        "-----------------------------------------\n"
+        "Tva volba: "
+    );
+}
+
+
+/** Volba pro obsluhu menu. */
+char zjistiVolbu(void)
+{
+    char volba;
+    scanf(" %c", &volba);
+
+    int c;
+    while((c = getchar()) != EOF && c != '\n') {}
+
+    return volba;
+}
+
+int main(void)
+{
+    Tstrom *strom = bvsInit();
+    int klic = 4;
+
+    char volba;
+    int konec = 0;
+
+    do {
+        menu();  // Zobrazí uživatelské menu
+        volba = zjistiVolbu();
+        switch (toupper(volba))
+        {
+            case '8': {
+                Tuzel *u = najdiMinimum(strom);
+                if (u != NULL)
+                    printf("Nejmensi prvek: %f\n", u->data);
+                else
+                    printf("Strom je prazdny.\n");
+                break;
+            }
+            case '9': {
+                Tuzel *u = najdiMaximum(strom);
+                if (u != NULL)
+                    printf("Nejmensi prvek: %f\n", u->data);
+                else
+                    printf("Strom je prazdny.\n");
+                break;
+            }
+            case 'A': {
+                Tuzel *u = bvsNajdi(strom, klic);
+                if (u != NULL)
+                    printf("Nejmensi prvek: %f\n", u->data);
+                else
+                    printf("Strom je prazdny.\n");
+                break;
+            }
+            case 'B':
+                vypisMensiNez(strom, klic);
+                break;
+
+            case 'C':
+                vypisVetsiNez(strom, klic);
+                break;
+
+            case 'D':
+                if (zrusVybranyList(strom, klic)) {
+                    printf("List byl zrusen\n");
+                } else {
+                    printf("List se nepodarilo zrusit\n");
+                }
+                break;
+
+            case 'E':
+                vypisInOrder(strom);
+                break;
+
+            case 'F':
+                vypisInOrderReverse(strom);
+                break;
+
+            case 'H':
+                printf("Vaha stromu: %d\n Vyska stromu: %d\n", bvsPruchodVaha(strom), vyskaStromu(strom));
+                break;
+
+            case 'I':
+                bvsZrus(strom);
+                strom = bvsInit();
+                vlozeni10Uzlu(strom);
+                printf("Strom byl zrusen a znovu inicializovan.\n");
+                break;
+
+            case 'T':
+                printf("Aktualni podoba stromu:\n");
+                SuperTisk(stdout, strom->koren);
+                printf("Pocet uzlu stromu: %d\n", bvsVaha(strom));
+                break;
+
+            case 'V':
+                {
+                    printf("Zadej hodnotu klice pro vlozeni (kladne cele cislo):\n");
+                    int klic;
+                    float data;
+                    scanf(" %d", &klic);
+
+                    printf("Zadej data ke klici %d (kladne desetinne cislo):\n", klic);
+                    scanf(" %f", &data);
+
+                    if (data < 0) data = -data;
+
+                    if (bvsVloz(strom, klic, data))
+                        printf("Uspech. Par (%d - %g) byl uspesne vlozen.\n", klic, data);
+                    else
+                        printf("Neuspech. Par (%d - %g) nebyl vlozen (duplikat?).\n", klic, data);
+                }
+                break;
+
+            case 'O':
+                {
+                    printf("Zadej hodnotu klice listu, ktery chces odebrat:\n");
+                    int klic;
+                    scanf(" %d", &klic);
+
+                    if (zrusVybranyList(strom, klic))
+                        printf("Uspech. Klic %d byl odebran ze stromu.\n", klic);
+                    else
+                        printf("Neuspech. Klic %d nebyl ve stromu nalezen.\n", klic);
+                }
+                break;
+
+            case 'K':
+                printf("Rusim strom a koncim.\n");
+                konec = 1;
+                break;
+
+            default:
+                printf("Neznamy prikaz. Zkus to znovu.\n");
+                break;
+        }
+
+    } while (!konec);
+
+    bvsZrus(strom);
+    return EXIT_SUCCESS;
+}
+
---
+++ 00-Banka-uloh/17_vyhledavaciAlgoritmy/strom/strom.c
@@ -0,0 +1,210 @@
+/*
+ * Projekt: Binární vyhledávací strom
+ * Autor: David Martinek
+ * Datum: 2020-11-11
+ * Popis
+ * Implementace funkcí realizující binární vyhledávací strom.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include "strom.h"
+
+
+
+
+Tstrom *bvsInit(void)
+{
+    Tstrom* strom = malloc(sizeof(Tstrom));
+    if (strom == NULL)
+    {
+        return NULL;
+    }
+
+    strom->koren = NULL;
+    strom->vaha = 0;
+    return strom;
+}
+
+
+
+bool bvsJePrazdny(Tstrom *strom)
+{
+    return strom->koren == NULL;
+}
+
+
+
+
+/** Zruší zadaný uzel. */
+bool _zrusUzel(Tuzel **uzel)
+{
+    Tuzel *pom = *uzel;
+    
+    if (pom == NULL) { return false; }
+
+    if (pom->levy == NULL) {
+        *uzel = pom->pravy;
+        free(pom);
+
+        return true;
+    }
+
+    if (pom->pravy == NULL) {
+        *uzel = pom->levy;
+        free(pom);
+
+        return true;
+    }
+
+    if (pom->levy->pravy == NULL) {
+        pom->klic = pom->levy->klic;
+
+        return _zrusUzel(&pom->levy);
+    }
+
+    pom = pom->levy;
+    while (pom->pravy->pravy != NULL) {
+        pom = pom->pravy;
+    }
+
+    (*uzel)->klic = pom->pravy->klic;
+    return _zrusUzel(&pom->pravy);
+}
+
+bool _bvsOdeber(Tuzel **u, int klic)
+{
+    Tuzel *uzel = *u;
+    if (uzel == NULL) return false;
+    if (klic == uzel->klic)
+    {
+        _zrusUzel(u); // tady se předává odkazem, i když to tak nevypadá! u se změní
+        return true;
+    }
+    else if (klic < uzel->klic)
+        return _bvsOdeber(&uzel->levy, klic);
+    else
+        return _bvsOdeber(&uzel->pravy, klic);
+}
+
+
+bool bvsOdeber(Tstrom *strom, int klic)
+{
+    if (_bvsOdeber(&strom->koren, klic))
+    {
+        strom->vaha -= 1;
+        return true;
+    }
+    return false;
+}
+
+
+/** Pomocna funkce pro tisk stromu. */
+unsigned int _bvsPom1(Tuzel *u)
+{
+    return (u == NULL)? 0 : _bvsPom1(u->levy) + _bvsPom1(u->pravy) + 1;
+}
+
+
+/** Pomocna funkce pro tisk stromu. */
+unsigned int _bvsPom2(Tuzel *u)
+{
+    if (u == NULL)
+        return 0;
+    else
+    {
+        unsigned int a = _bvsPom2(u->levy);
+        unsigned int b = _bvsPom2(u->pravy);
+        return 1 + ((a > b)? a:b);
+    }
+}
+
+
+/**
+ * Pomocná funkce sloužící k ladícímu výpisu binárního stromu.
+ * Do pole uzly uloží ukazatele na uzly ve správném pořadí podle klíčů.
+ * Do pole uroven vloží úroveň, ve které se nachází uzel z pole uzly.
+ */
+void _stromNaPole(Tuzel *uzel, Tuzel *uzly[], unsigned int uroven[], int *index, int vyska)
+{
+    if (uzel == NULL) return;
+    _stromNaPole(uzel->levy, uzly, uroven, index, vyska+1);
+    uzly[*index] = uzel;
+    uroven[*index] = vyska;
+    (*index)++;
+    _stromNaPole(uzel->pravy, uzly, uroven, index, vyska+1);
+}
+
+
+/**
+ * Vytiskne hodnoty uložené ve stromu tak, že každou úroveň stromu vytiskne
+ * na jeden řádek. Použitelné pro ladící účely. Pozor! Strom musí být ve
+ * stavu, kdy se jím dá bezpečně procházet. Pokud listy nemají své podstromy
+ * nastaveny na NULL, ale jsou v nich náhodné adresy, skončí tato operace
+ * havárií.
+ */
+void bvsTisk(Tstrom *strom)
+{
+    if (strom->koren == NULL)
+    {
+        printf("NULL\n");
+        return;
+    }
+    printf("\n\n");
+    unsigned int mm = _bvsPom1(strom->koren);
+    unsigned int nn = _bvsPom2(strom->koren);
+
+    Tuzel *uzly[mm];
+    unsigned int uroven[mm];
+    int index = 0;
+
+    _stromNaPole(strom->koren, uzly, uroven, &index, 0);
+
+    for (unsigned int u = 0; u < nn; u++)
+    {
+        printf("Uroven %2d: ", u);
+        for(unsigned int i = 0; i < mm; i++)
+        {
+            if(uroven[i] == u)
+            {
+                printf("%s[%4d]%s",
+                        (uzly[i]->levy == NULL)? "|-" : "<-",
+                        uzly[i]->klic,
+                        (uzly[i]->pravy == NULL)? "-|" : "->");
+            }
+            else
+            {
+                printf("          ");
+            }
+        }
+        printf("\n");
+    }
+}
+
+void _preOrder(Tuzel * u)
+{
+    if (u == NULL) { return; }
+
+    printf("%d - %f\n", u->klic, u->data);
+    _preOrder(u->levy);
+    _preOrder(u->pravy);
+}
+
+void vypisPreOrder(Tstrom * strom)
+{
+    _preOrder(strom->koren);
+}
+
+void _postOrder(Tuzel * u)
+{
+    if (u == NULL) { return; }
+
+    _postOrder(u->levy);
+    _postOrder(u->pravy);
+    printf("%d - %f\n", u->klic, u->data);
+}
+
+void vypisPostOrder(Tstrom * strom)
+{
+    _postOrder(strom->koren);
+}
+
