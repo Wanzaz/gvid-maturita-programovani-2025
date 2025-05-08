ee9a237266866c95c0f30f7e37648865c07e9582
Wanzaz <ondra.pazourek@gmail.com>
2025-05-08 16:33:21 +0200

    Fix efficiency

--- 00-Banka-uloh/10_HZD/primarniCiziKlice/main.c
+++ 00-Banka-uloh/10_HZD/primarniCiziKlice/main.c
@@ -4,7 +4,6 @@
 
 #define JMENO_DELKA 10
 #define EMAIL_DELKA 20
-#define BUFFER_SIZE 256
 
 typedef struct {
     int id;
@@ -17,25 +16,20 @@ typedef struct {
 } TEmailZaznam;
 
 void vypisEmailProJmeno(const char *jmeno_k_vyhledani, FILE *f_uzivatelu, FILE *f_emailu) {
-    char radek_uzivatelu[BUFFER_SIZE];
-    char radek_emailu[BUFFER_SIZE];
     TUzivatel uzivatel;
     TEmailZaznam email_zaznam;
 
     printf("Výsledky pro jméno '%s':\n", jmeno_k_vyhledani);
 
-    while (fgets(radek_uzivatelu, BUFFER_SIZE, f_uzivatelu) != NULL) {
-        if (sscanf(radek_uzivatelu, "%d %10s", &uzivatel.id, uzivatel.jmeno) == 2) {
-            if (strcmp(uzivatel.jmeno, jmeno_k_vyhledani) == 0) {
-                rewind(f_emailu);
-                while (fgets(radek_emailu, BUFFER_SIZE, f_emailu) != NULL) {
-                    if (sscanf(radek_emailu, "%d %20s", &email_zaznam.id_uzivatele, email_zaznam.email) == 2) {
-                        if (email_zaznam.id_uzivatele == uzivatel.id) {
-                            printf("%d %s %s\n", uzivatel.id, uzivatel.jmeno, email_zaznam.email);
-                        }
-                    }
-                }
-            }
+    while (fscanf(f_uzivatelu, "%d %10s", &uzivatel.id, uzivatel.jmeno) == 2) {
+        if (strcmp(uzivatel.jmeno, jmeno_k_vyhledani) == 0) {
+            break;
+        }
+    }
+
+    while (fscanf(f_emailu, "%d %20s", &email_zaznam.id_uzivatele, email_zaznam.email) == 2) {
+        if (email_zaznam.id_uzivatele == uzivatel.id) {
+            printf("%d %s %s\n", uzivatel.id, uzivatel.jmeno, email_zaznam.email);
         }
     }
 }
@@ -44,8 +38,6 @@ int main() {
     char soubor_uzivatelu[256];
     char soubor_emailu[256];
     char hledane_jmeno[JMENO_DELKA + 1];
-    FILE *f_uzivatelu = NULL;
-    FILE *f_emailu = NULL;
 
     printf("Zadejte název souboru s uživateli: ");
     if (scanf("%255s", soubor_uzivatelu) != 1) {
@@ -65,8 +57,8 @@ int main() {
         return 1;
     }
 
-    f_uzivatelu = fopen(soubor_uzivatelu, "r");
-    f_emailu = fopen(soubor_emailu, "r");
+    FILE *f_uzivatelu = fopen(soubor_uzivatelu, "r");
+    FILE *f_emailu = fopen(soubor_emailu, "r");
 
     if (f_uzivatelu == NULL || f_emailu == NULL) {
         perror("Chyba při otevírání souborů");
